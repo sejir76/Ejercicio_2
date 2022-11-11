39add2c9fe1754f678029fa88b36907777445d28
rafa <homer512x@gmail.com>
2022-11-11 16:45:00 +0100

    archivo main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+//Variable con mi nombre:
+
+var nombre = "Rafa"
+
+//Variable con mi edad
+
+var edad = 46
+
+//Variable mayor de edad booleana
+
+var mayorDeEdad = true
+
+//Crea una variable llamada dirección y asígnale un objeto con tu dirección
+
+var address = {
+	direccion: "Calle panocha",
+}
+
+const calle = address.direccion
+
+//Crea una variable llamada coloresFavoritos y asígnale un array con tus colores favoritos
+
+var coloresFavoritos = [Rojo, Violeta, Azul, Negro]
+
+//Crea una variable llamada lenguajesFavoritos y asígnale una array con tus lenguajes de programación
+
+var lenguajesFavoritos = [Html, Css, JavaScript, Python]
+
+//Crea una variable llamada mejorLenguaje y asígnale el primer elemento del array lenguajesFavoritos
+
+const mejorLenguaje = lenguajesFavoritos[0]
+
+//Crea una variable llamada peorLenguaje y asígnale el último elemento del array lenguajesFavoritos
+
+let peorLenguaje = lenguajesFavoritos[3]
